dd86a443d4ee68dd174917092ee31b9979d53205
venkata charan <charan.kata80@gmail.com>
2023-05-27 14:46:37 +0530

    added

---
+++ Blob.h
@@ -0,0 +1,39 @@
+#ifndef MY_BLOB
+#define MY_BLOB
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+class Blob {
+public:
+    // member variables ///////////////////////////////////////////////////////////////////////////
+    vector<Point> currentContour;
+
+    Rect currentBoundingRect;
+
+    vector<Point> centerPositions;
+
+    double dblCurrentDiagonalSize;
+    double dblCurrentAspectRatio;
+
+    bool blnCurrentMatchFoundOrNewBlob;
+
+    bool blnStillBeingTracked;
+
+    int intNumOfConsecutiveFramesWithoutAMatch;
+
+    Point predictedNextPosition;
+    
+    // function prototypes ////////////////////////////////////////////////////////////////////////
+    Blob(vector<Point> _contour);
+    void predictNextPosition(void);
+
+};
+
+#endif    // MY_BLOB
